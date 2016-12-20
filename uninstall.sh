8b8e24fe062601a9e7a22db226012be4366bba25
jaroklima <bitbucket@jaroslavklima.com>
2016-12-20 17:53:12 +0100

    add dummy VERSION to uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -3,13 +3,14 @@
 which docker
 
 if [ $? = 0 ]; then
-        PATH="$PATH:/usr/local/bin"
+  PATH="$PATH:/usr/local/bin"
+  export VERSION="dummy";
 
-        docker-compose stop
-        docker-compose rm -f
-        docker rmi --force $(docker images -a | grep "^ubnt/unms" | awk '{print $3}')
+  docker-compose stop
+  docker-compose rm -f
+  docker rmi --force $(docker images -a | grep "^ubnt/unms" | awk '{print $3}')
 
-        echo "Removed UNMS docker containers and images."
+  echo "Removed UNMS docker containers and images."
 else
-        echo "Docker not installed, nothing to uninstall."
+  echo "Docker not installed, nothing to uninstall."
 fi
