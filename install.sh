9fc3c7564ac587bb06d630a737ec932b9ecfae07
Jaroslav Klima <jaroklima@users.noreply.github.com>
2017-08-14 11:37:21 +0200

    refactor(installation): install.sh script improvements (#67)
    Ref UNMS-1556

--- install.sh
+++ install.sh
@@ -10,45 +10,45 @@ version=""
 branch="master"
 
 branchRegex=" --branch ([^ ]+)"
-if [[ " $args" =~ $branchRegex ]]; then
+if [[ " ${args}" =~ ${branchRegex} ]]; then
   branch="${BASH_REMATCH[1]}"
 fi
-echo branch="$branch"
+echo "branch=${branch}"
 
 repo="https://raw.githubusercontent.com/Ubiquiti-App/UNMS/${branch}"
 
 versionRegex=" --version ([^ ]+)"
-if [[ " $args" =~ $versionRegex ]]; then
+if [[ " ${args}" =~ ${versionRegex} ]]; then
   version="${BASH_REMATCH[1]}"
 fi
 
-if [ -z "$version" ]; then
-  latestVersionUrl="$repo/latest-version"
+if [ -z "${version}" ]; then
+  latestVersionUrl="${repo}/latest-version"
   if ! version=$(curl -fsS "${latestVersionUrl}"); then
-    echo "Failed to obtain latest version info from $latestVersionUrl"
+    echo >&2 "Failed to obtain latest version info from ${latestVersionUrl}"
     exit 1
   fi
 fi
-echo version="$version"
+echo version="${version}"
 
-rm -rf $temp
-if ! mkdir $temp; then
-  echo "Failed to create temporary directory"
+rm -rf "${temp}"
+if ! mkdir "${temp}"; then
+  echo >&2 "Failed to create temporary directory"
   exit 1
 fi
 
-cd $temp
-echo "Downloading installation package for version $version."
-packageUrl="$repo/unms-$version.tar.gz"
+cd "${temp}"
+echo "Downloading installation package for version ${version}."
+packageUrl="${repo}/unms-${version}.tar.gz"
 if ! curl -sS "${packageUrl}" | tar xz; then
-  echo "Failed to download installation package ${packageUrl}"
+  echo >&2 "Failed to download installation package ${packageUrl}"
   exit 1
 fi
 
 chmod +x install-full.sh
-./install-full.sh $args --version "$version"
+./install-full.sh ${args} --version "${version}"
 
 cd ~
-if ! rm -rf $temp; then
-  echo "Warning: Failed to remove temporary directory $temp"
+if ! rm -rf "${temp}"; then
+  echo >&2 "Warning: Failed to remove temporary directory ${temp}"
 fi
