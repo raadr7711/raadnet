cdf0a26dace8b83c14fe1469c81ce76e9f7c07d7
jaroklima <git@jaroslavklima.com>
2017-09-11 14:07:44 +0200

    get-statistics script

--- scripts/get-statistics.sh
+++ scripts/get-statistics.sh
@@ -3,6 +3,9 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+outdir=~unms/device-statistics
+outfile=~unms/device-statistics.tar.gz
+
 deviceId="$1"
 
 if [ -z "${deviceId}" ]; then
@@ -10,7 +13,13 @@ if [ -z "${deviceId}" ]; then
   exit 1
 fi
 
-docker exec -t unms-redis redis-cli KEYS statistics\* | grep "${deviceId}" | cut -d\" -f2 | xargs -i% sh -c 'sudo docker exec -t unms-redis redis-cli LRANGE % 0 -1 >%.txt'
-tar --remove-files -czf statistics.tar.gz statistics*.txt
+mkdir -p "${outdir}"
+keys=( $(docker exec -t unms-redis redis-cli KEYS statistics\* | grep "${deviceId}" | cut -d\" -f2) )
 
-echo "Statistics saved to statistics.tar.gz"
+for key in "${keys[@]}"; do
+  docker exec -t unms-redis redis-cli LRANGE "${key}" 0 -1 >${outdir}/${key}
+done
+
+tar --remove-files -C "${outdir}" -czf "${outfile}" .
+
+echo "Saved to ${outfile}"
